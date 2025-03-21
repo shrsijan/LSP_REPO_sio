65089fa8601b224e5166e685e4b948c1d7b13c65
Sijan Shrestha <84898822+shrsijan@users.noreply.github.com>
2025-03-20 20:19:21 -0400

    Question2

--- LSP_sping_2025/src/org/howard/edu/lsp/midterm/question2/Book.java
+++ LSP_sping_2025/src/org/howard/edu/lsp/midterm/question2/Book.java
@@ -1,5 +1,4 @@
 package org.howard.edu.lsp.midterm.question2;
-
 /**
  * Beginning of question 2 midterm.
  * Initiating a class book in a library with a title, author, ISBN, and year published.
@@ -115,28 +114,5 @@ public class Book {
     @Override
     public String toString() {
         return "Title: " + title + ", Author: " + author + ", ISBN: " + ISBN + ", Year Published: " + yearPublished;
-    }
-}
-
-/**
- * The BookDriver class demonstrates the functionality of the Book class.
- * It creates book instances, checks equality, and prints book details.
- */
-class BookDriver {
-    /**
-     * The main method to execute the Book class functionality.
-     * @param args Command-line arguments (not used).
-     */
-    public static void main(String[] args) {
-        // Creating book objects
-        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "123456789", 1925);
-        Book book2 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "123456789", 1925);
-        Book book3 = new Book("A Promised Land", "Barack Obama", "9780593239681", 2020);
-
-        // Demonstrating the equals method
-        System.out.println(book1.equals(book2));
-        System.out.println(book1.equals(book3));
-        // Printing book details
-        System.out.println(book1);
     }
 }
